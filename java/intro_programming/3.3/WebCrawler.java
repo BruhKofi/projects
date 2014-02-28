41ab2825c216ffdfd3c8003e42d17db8aa6cba39
MatthewPhinney <matthew.d.phinney@gmail.com>
2014-02-28 09:37:19 -0500

    Updating web crawler

--- java/intro_programming/3.3/WebCrawler.java
+++ java/intro_programming/3.3/WebCrawler.java
@@ -4,68 +4,37 @@ public class WebCrawler
     private static final int k = 5;
     
     public static void main(String[] args) {
-        // String startPage = args[0];
-        // String comparePage = args[1];
-        // double tol = Double.parseDouble(args[2]);
-        // Document start = new Document(startPage, k, d);
-        // Document fixedPoint = new Document(comparePage, k, d);
-        // WebCrawler wb = new WebCrawler();
-        // wb.crawl(fixedPoint, start, tol, 0);
-
-        //test nextURL method
-        WebCrawler wc = new WebCrawler();
-        Document doc = new Document("http://www.nytimes.com", k, d);
-        int i = 0;
-        while (true) {
-            URLReader r = wc.nextURL(doc, i);
-            if (r.indexOfURL == -1) break;
-            i = r.indexOfURL + 6;
-            StdOut.println(r.URL);
-        }
+        // test crawl method
+        Document d1 = new Document(args[0], k, d);
+        Document d2 = new Document(args[1], k, d);
+        double tol = Double.parseDouble(args[2]);
+        crawl(d1, d2, tol);
     }
 
-    public void crawl(Document d1, Document d2, double tol, int i) {
+    public static void crawl(Document d1, Document d2, double tol) {
         if (d1.simTo(d2) < 0.01) return;
-        if (d1.simTo(d2) > tol) StdOut.println(d2.name());
-        StdOut.println(d1.name() + " " + d2.name() + " " + d1.simTo(d2));
-        while (true) {
-            URLReader urlReader = nextURL(d2, i);
-            if (urlReader == null) return;
-            i = urlReader.indexOfURL + 6;
-            StdOut.println("URLReader name: " + urlReader.URL);
-            d2 = new Document(urlReader.URL, k, d);
-            crawl(d1, d2, tol, i);
-        }
-    }
-
-    private URLReader nextURL(Document d, int i) {
-        StdOut.println(i);
-        String documentText = (new In(d.name())).readAll();
+        //        if (d1.simTo(d2) > tol) StdOut.println(d1.name() + " " + d2.name() + " " + d1.simTo(d2));
+        String webText = (new In(d2.name())).readAll();
+        int N = webText.length();
         String start = "href=\"http://www";
+        int S = start.length();;
         String end = "\"";
-        i = documentText.indexOf(start, i);
-        if (i == -1) return new URLReader("", -1);
-        int j = documentText.indexOf(end, i+6);
-        URLReader urlReader = new URLReader();
-        if ("html".equals(documentText.substring(j-4, j))) {
-            StdOut.println(documentText.substring(i+6, j));
-            String URL = documentText.substring(i+6, j);
-            urlReader.URL = URL;
-            urlReader.indexOfURL = i;
+        int E = end.length();;
 
-        } else {
-            nextURL(d, j);
-        }
-        return urlReader;
-    }
-
-    private class URLReader {
-        String URL;
-        int indexOfURL;
-        private URLReader() {};
-        private URLReader(String url, int index) {
-            URL = url;
-            indexOfURL = index;
+        int beg = -1;
+        for (int i = 0; i<N-S; i++) {
+            String startCodon = webText.substring(i, i+S);
+            String endCodon = webText.substring(i, i+E);
+            if (start.equals(startCodon)) beg = i;
+            if (end.equals(endCodon) && beg != -1 && i - beg > 6) {
+                String URL = webText.substring(beg+6, i);
+                if (!d2.name().equals(URL)) {
+                    StdOut.println(URL);
+                    d2 = new Document(URL, k, d);
+                    crawl(d1, d2, tol);
+                }
+                beg = -1;
+            }
         }
     }
 }
