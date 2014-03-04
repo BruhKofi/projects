b81d1e514bb2f6db819e7ac2fca3ed1aec3e4fc1
MatthewPhinney <matthew.d.phinney@gmail.com>
2014-03-04 13:47:16 -0500

    Adding const space lin time method for k-rotation of char array

--- java/intro_programming/4.1/StringRotation.java
+++ java/intro_programming/4.1/StringRotation.java
@@ -2,8 +2,10 @@ public class StringRotation
 {
     public static String rotate(String s, int index) {
         int N = s.length();
+        int k = index % N;
+        if (k == 0) return s;
         String r = reverse(s);
-        return reverse(r.substring(0, N - index)) + reverse(r.substring(N-index, N));
+        return reverse(r.substring(0, N-k)) + reverse(r.substring(N-k, N));
     }
 
     private static String reverse(String s) {
@@ -15,10 +17,34 @@ public class StringRotation
         return new String(a);
     }
 
+    public static void rotate(char[] x, int index) {
+        int N = x.length;
+        int k = index % N;
+        if (k == 0) return;
+        reverse(x, 0, N);
+        reverse(x, 0, N-k);
+        reverse(x, N-k, N);
+    }
+
+    private static void reverse(char[] x, int lo, int hi) {
+        for (int i = lo; i <  lo + (hi - lo)/2; i++) {
+            swap(x, i, hi + lo - i - 1);
+        }
+    }
+
+    private static void swap(char[] x, int i, int j) {
+        char t = x[i];
+        x[i] = x[j];
+        x[j] = t;
+    }
+
     public static void main(String[] args) {
         String s = args[0];
         for (int i = 0; i<s.length(); i++) {
-            StdOut.println(rotate(s, i));
+            char[] x = s.toCharArray();
+            rotate(x, i);
+            StdOut.println("character array\t\t" + new String(x));
+            StdOut.println("string\t\t\t" + rotate(s, i));
         }
     }
 }
