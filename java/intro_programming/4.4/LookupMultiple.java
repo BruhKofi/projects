6bd209f1ac781768b863f5ec46a79a5675e58588
MatthewPhinney <matthew.d.phinney@gmail.com>
2014-04-01 09:56:43 -0400

    Adding an ST<String, Queue<String>> client

---
+++ java/intro_programming/4.4/LookupMultiple.java
@@ -0,0 +1,34 @@
+public class LookupMultiple
+{
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        int keyField = Integer.parseInt(args[1]);
+        int valField = Integer.parseInt(args[2]);
+
+        String[] database = in.readAll().split("\n");
+        StdRandom.shuffle(database);
+
+        BST<String, Queue<String>> st = new BST<String, Queue<String>>();
+        for (int i = 0; i<database.length; i++) {
+            String[] tokens = database[i].split(",");
+            String key = tokens[keyField];
+            String val = tokens[valField];
+            if (!st.contains(key)) {
+                st.put(key, new Queue<String>());
+            }
+            Queue<String> q = st.get(key);
+            q.enqueue(val);
+        }
+
+        while (!StdIn.isEmpty()) {
+            String s = StdIn.readString();
+            if (st.contains(s)) {
+                Queue<String> q = st.get(s);
+                for (String t : q) { StdOut.print(t + " "); }
+                StdOut.println();
+            } else {
+                StdOut.println("Not found");
+            }
+        }
+    }
+}
