50f094788c058d7f13d5de1a8f91ddb4b00fd730
MatthewPhinney <matthew.d.phinney@gmail.com>
2014-04-05 14:26:36 -0400

    Adding linear time methods to find the size, num leaves, and key sum of binary tree

---
+++ java/intro_programming/4.4/BSTPractice.java
@@ -0,0 +1,84 @@
+public class BSTPractice
+{
+    private Node root;
+
+    private class Node
+    {
+        final double d;
+        Node left;
+        Node right;
+
+        Node(double d) { this.d = d; }
+
+        boolean isLeaf() {
+            return left == null && right == null;
+        }
+    }
+
+    public void put(double d) {
+        root = put(root, d);
+    }
+
+    private Node put(Node x, double d) {
+        if (x == null) return new Node(d);
+        if (d < x.d) x.left = put(x.left, d);
+        if (d > x.d) x.right = put(x.right, d);
+        return x;
+    }
+
+
+    public int size() {
+        return size(root);
+    }
+
+    private int size(Node x) {
+        if (x == null) return 0;
+        if (x.isLeaf()) return 1;
+        return 1 + size(x.left) + size(x.right);
+    }
+
+    public int leaves() {
+        return leaves(root);
+    }
+
+    private int leaves(Node x) {
+        if (x == null) return 0;
+        if (x.isLeaf()) return 1;
+        return leaves(x.left) + leaves(x.right);
+    }
+
+    public double total() {
+        return total(root);
+    }
+
+    private double total(Node x) {
+        if (x == null) return 0;
+        if (x.isLeaf()) return x.d;
+        return x.d + total(x.left) + total(x.right);
+    }
+
+    public static void main(String[] args) {
+        // BSTPractice st = new BSTPractice();
+        // while (!StdIn.isEmpty()) {
+        //     double d = StdIn.readDouble();
+        //     st.put(d);
+        // }
+        // StdOut.println("size: " + st.size() + "\nleaves: " + st.leaves() + "\ntotal: " + st.total());
+         Stopwatch sw = new Stopwatch();
+         for (int i = 100; true; i*=2) {
+             BSTPractice st = new BSTPractice();
+             for (int j = 0; j<i; j++) {
+                 st.put(StdRandom.uniform());
+             }
+             sw = new Stopwatch();
+             int n = st.size();
+             StdOut.println(i + ": " + sw.elapsedTime());
+             sw = new Stopwatch();
+             n = st.leaves();
+             StdOut.println(i + ": " + sw.elapsedTime());
+             sw = new Stopwatch();
+             double d = st.total();
+             StdOut.println(i + ": " + sw.elapsedTime());
+         }
+    }
+}
