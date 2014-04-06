182e62b5f1c647c74c58cbc33226b313d35f81a1
MatthewPhinney <matthew.d.phinney@gmail.com>
2014-04-06 11:38:32 -0400

    Adding frequency table implementation

---
+++ java/intro_programming/4.4/FrequencyTable.java
@@ -0,0 +1,25 @@
+public class FrequencyTable
+{
+    BST<String, Integer> st = new BST<String, Integer>();
+    
+    public void click(String s) {
+        if (st.contains(s)) st.put(s, st.get(s)+1);
+        else st.put(s, 1);
+    }
+
+    public int count(String s) {
+        if (!st.contains(s)) return 0;
+        return st.get(s);
+    }
+
+    public static void main(String[] args) {
+        FrequencyTable fq = new FrequencyTable();
+
+        fq.click("hello");
+        fq.click("hello");
+        fq.click("world");
+
+        StdOut.println(fq.count("hello") + " " + fq.count("world") + " " + fq.count("none"));
+    }
+}
+        
