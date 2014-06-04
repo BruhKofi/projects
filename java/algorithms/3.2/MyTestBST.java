83b14d7e46e6dc9d42cf923a9523308189b5e951
MatthewPhinney <matthew.d.phinney@gmail.com>
2014-06-04 18:40:43 -0400

    Updating BST implementation

--- java/algorithms/3.2/MyTestBST.java
+++ java/algorithms/3.2/MyTestBST.java
@@ -58,6 +58,87 @@ public class MyTestBST<Key extends Comparable<Key>, Value>
         else return 1 + Math.max(height(x.left), height(x.right));
     }
 
+    public void delete(Key key) {
+        root = delete(root, key);
+    }
+
+    private Node delete(Node x, Key key) {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) x.left = delete(x.left, key);
+        else if (cmp > 0) x.right = delete(x.right, key);
+        else {
+            if (x.right == null) return x.left;
+            if (x.left == null) return x.right;
+            Node t = x;
+            x = min(t.right);
+            x.right = deleteMin(t.right);
+            x.left = t.left;
+        }
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    public Key min() {
+        return min(root).key;
+    }
+
+    private Node min(Node x) {
+        if (x == null) return null;
+        if (x.left == null) return x;
+        return min(x.left);
+    }
+
+    public Key max() {
+        return max(root).key;
+    }
+
+    private Node max(Node x) {
+        if (x == null) return null;
+        if (x.right == null) return x;
+        return max(x.right);
+    }
+
+    public int rank(Key key) {
+        return rank(root, key);
+    }
+
+    private int rank(Node x, Key key) {
+        if (x == null) return 0;
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) return rank(x.left, key);
+        else if (cmp > 0) return 1 + size(x.left) + rank(x.right, key);
+        else return size(x.left);
+    }
+
+    public Key select(int k) {
+        return select(root, k).key;
+    }
+
+    private Node select(Node x, int k) {
+        if (x == null) return null;
+        int t = size(x.left);
+        if (t > k) return select(x.left, k);
+        else if (t < k) return select(x.right, k - 1 - t);
+        else return x;
+    }
+
+    public void deleteMin() {
+        root = deleteMin(root);
+    }
+
+    private Node deleteMin(Node x) {
+        if (x == null) return null;
+        if (x.left == null) return x.right;
+        x.left = deleteMin(x.left);
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
     public static void main(String[] args) {
         MyTestBST<String, String> st = new MyTestBST<String, String>();
         while (!StdIn.isEmpty()) {
