b30f65c7b7442bc3cc8d72e56e92a764871b34f8
MatthewPhinney <matthew.d.phinney@gmail.com>
2014-09-02 15:15:37 -0400

    ADT for graph processing

---
+++ java/algorithms/4.1/MySymbolGraph.java
@@ -0,0 +1,48 @@
+public class MySymbolGraph
+{
+    private ST<String, Integer> st;
+    private String[] keys;
+    private MyGraph G;
+
+    public MySymbolGraph(String stream, String sp) {
+        st = new ST<String, Integer>();
+        In in = new In(stream);
+        while (in.hasNextLine()) {
+            String[] a = in.readLine().split(sp);
+            for (int i = 0; i<a.length; i++) {
+                if (!st.contains(a[i])) st.put(a[i], st.size());
+            }
+        }
+
+        keys = new String[st.size()];
+        for (String name : st.keys()) {
+            keys[st.get(name)] = name;
+        }
+
+        G = new MyGraph(st.size());
+        in = new In(stream);
+        while (in.hasNextLine()) {
+            String[] a = in.readLine().split(sp);
+            int v = st.get(a[0]);
+            for (int i = 1; i<a.length; i++) {
+                G.addEdge(v, st.get(a[i]));
+            }
+        }
+    }
+
+    public boolean contains(String s) {
+        return st.contains(s);
+    }
+
+    public int index(String s) {
+        return st.get(s);
+    }
+
+    public String name(int index) {
+        return keys[index];
+    }
+
+    public MyGraph G() {
+        return G;
+    }
+}
