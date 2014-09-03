8f9a98b780417cc0b695295c6252f1b816b557c9
MatthewPhinney <matthew.d.phinney@gmail.com>
2014-09-03 11:09:37 -0400

    Program to computer center, radius, diameter of graph

---
+++ java/algorithms/4.1/MyGraphProperties.java
@@ -0,0 +1,68 @@
+import java.util.InputMismatchException;
+public class MyGraphProperties
+{
+    private int[] eccentricity;
+    private int diameter;
+    private int radius;
+    private int center;
+
+    public MyGraphProperties(MyGraph G) {
+        MyConnectedComponents cc = new MyConnectedComponents(G);
+        if (cc.count() != 1) throw new InputMismatchException("Graph must be connected");
+        eccentricity = new int[G.V()];
+        eccentricities(G);
+        findDiameter();
+        findRadius();
+        findCenter();
+    }
+
+    private void eccentricities(MyGraph G) {
+        for (int v = 0; v<G.V(); v++) {
+            MyBreadthFirstPaths bfs = new MyBreadthFirstPaths(G, v);
+            int eccen = Integer.MIN_VALUE;
+            for (int w = 0; w<G.V(); w++) {
+                if (bfs.distTo(w) > eccen) eccen = bfs.distTo(w);
+            }
+            eccentricity[v] = eccen;
+        }
+    }
+
+    private void findDiameter() {
+        diameter = StdStats.max(eccentricity);
+    }
+
+    private void findRadius() {
+        radius = StdStats.min(eccentricity);
+    }
+
+    private void findCenter() {
+        for (int i = 0; i<eccentricity.length; i++) {
+            if (eccentricity[i] == radius) {
+                center = i;
+                return;
+            }
+        }
+        center = -1;
+    }
+
+    public int radius() {
+        return radius;
+    }
+
+    public int diameter() {
+        return diameter;
+    }
+
+    public int center() {
+        return center;
+    }
+
+    public static void main(String[] args) {
+        MyGraph G = new MyGraph(new In(args[0]));
+        MyGraphProperties gp = new MyGraphProperties(G);
+        StdOut.println("Diameter: " + gp.diameter());
+        StdOut.println("Radius: " + gp.radius());
+        StdOut.println("Center: " + gp.center());
+    }
+}
+    
