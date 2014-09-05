c43f51955df2aa6ba0197c9f99925466c6feaa70
MatthewPhinney <matthew.d.phinney@gmail.com>
2014-09-05 13:33:28 -0400

    single source paths implementation

---
+++ java/algorithms/4.2/MyDirectedPaths.java
@@ -0,0 +1,50 @@
+public class MyDirectedPaths
+{
+    private boolean[] marked;
+    private int[] edgeTo;
+    private int s;
+
+
+    public MyDirectedPaths(MyDigraph G, int s) {
+        this.s = s;
+        marked = new boolean[G.V()];
+        edgeTo = new int[G.V()];
+        dfs(G, s);
+    }
+
+    private void dfs(MyDigraph G, int v) {
+        marked[v] = true;
+        for (int w : G.adj(v)) {
+            if (!marked[w]) {
+                edgeTo[w] = v;
+                dfs(G, w);
+            }
+        }
+    }
+
+    public Iterable<Integer> pathTo(int v) {
+        if (!marked[v]) return null;
+        Stack<Integer> stack = new Stack<Integer>();
+        for (int x = v; x != s; x = edgeTo[x]) stack.push(x);
+        stack.push(s);
+        return stack;
+    }
+
+    public boolean reachable(int v) {
+        return marked[v];
+    }
+
+    public static void main(String[] args) {
+        MyDigraph G = new MyDigraph(new In(args[0]));
+        MyDirectedPaths paths = new MyDirectedPaths(G, Integer.parseInt(args[1]));
+        while (!StdIn.isEmpty()) {
+            int v = StdIn.readInt();
+            if (paths.reachable(v)) {
+                for (int w : paths.pathTo(v)) StdOut.print(w + " ");
+                StdOut.println();
+            } else {
+                StdOut.println("Vertex " + v + " not reachable");
+            }
+        }
+    }
+}
