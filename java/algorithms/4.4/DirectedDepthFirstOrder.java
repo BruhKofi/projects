14ac4a362b674054cf616c0fe33caea95992fb07
MatthewPhinney <matthew.d.phinney@gmail.com>
2014-09-09 11:40:48 -0400

    pre, post, rev post order

---
+++ java/algorithms/4.4/DirectedDepthFirstOrder.java
@@ -0,0 +1,45 @@
+public class DirectedDepthFirstOrder
+{
+    private Queue<Integer> pre;
+    private Queue<Integer> post;
+    private Stack<Integer> reversePost;
+    public boolean[] marked;
+
+    public DirectedDepthFirstOrder(MyDirectedEdgeWeightedGraph G) {
+        marked = new boolean[G.V()];
+        pre = new Queue<Integer>();
+        post = new Queue<Integer>();
+        reversePost = new Stack<Integer>();
+
+        for (int v = 0; v<G.V(); v++) {
+            if (!marked[v]) {
+                dfs(G, v);
+            }
+        }
+    }
+
+    private void dfs(MyDirectedEdgeWeightedGraph G, int v) {
+        marked[v] = true;
+        pre.enqueue(v);
+        for (MyDirectedEdge e : G.adj(v)) {
+            int w = e.to();
+            if (!marked[w]) {
+                dfs(G, w);
+            }
+        }
+        post.enqueue(v);
+        reversePost.push(v);
+    }
+
+    public Iterable<Integer> pre() {
+        return pre;
+    }
+
+    public Iterable<Integer> post() {
+        return post;
+    }
+
+    public Iterable<Integer> reversePost() {
+        return reversePost;
+    }
+}
