db71a4e78e9142b258bac6b59f13d768a287c9d0
MatthewPhinney <matthew.d.phinney@gmail.com>
2014-09-13 15:51:16 -0400

    Quicksort with 3 way partitioning for strings

---
+++ java/algorithms/5.1/MyQuick3String.java
@@ -0,0 +1,44 @@
+public class MyQuick3String
+{
+    private static int charAt(String s, int d) {
+        if (d < s.length()) return s.charAt(d);
+        return -1;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    public static void sort(String[] a) {
+        sort(a, 0, a.length-1, 0);
+    }
+
+    private static void sort(String[] a, int lo, int hi, int d) {
+        if (hi <= lo) return;
+
+        int lt = lo, gt = hi;
+        int v = charAt(a[lo], d);
+        int i = lo+1;
+        while (i <= gt) {
+            int t = charAt(a[i], d);
+            if (t < v) exch(a, lt++, i++);
+            else if (t > v) exch(a, i, gt--);
+            else i++;
+        }
+
+        sort(a, lo, lt-1, d);
+        if (v >= 0) sort(a, lt, gt, d+1);
+        sort(a, gt+1, hi, d);
+    }
+
+    public static void main(String[] args) {
+        String[] a = StdIn.readAll().split("\\s+");
+        sort(a);
+        for (String s : a) {
+            StdOut.println(s);
+        }
+    }
+}
+            
