82b2269811b7adc6302aca86246ffbc70b76198d
MatthewPhinney <matthew.d.phinney@gmail.com>
2014-09-16 13:06:31 -0400

    Performance driver for tries (runs out of memory)

---
+++ java/algorithms/5.2/TrieTester.java
@@ -0,0 +1,36 @@
+public class TrieTester
+{
+    public static void main(String[] args) {
+        java.util.TreeMap<String, Integer> treeMap = new java.util.TreeMap<String, Integer>();
+        TrieST<Integer> trie = new TrieST<Integer>();
+        In in = new In(args[0]);
+        while (!in.isEmpty()) {
+            String s = in.readString();
+            if (treeMap.containsKey(s)) treeMap.put(s, treeMap.get(s)+1);
+            else treeMap.put(s, 1);
+        }
+        // for (String s : treeMap.keySet()) {
+        //     trie.put(s, treeMap.get(s));
+        // }
+
+        in = new In(args[1]);
+        Stopwatch sw = new Stopwatch();
+        while (!in.isEmpty()) {
+            String s = in.readString();
+            if (treeMap.containsKey(s)) {
+                int t = treeMap.get(s);
+            }
+        }
+        StdOut.println("Tree map took : " + sw.elapsedTime());
+
+        // in = new In(args[1]);
+        // sw = new Stopwatch();
+        // while (!in.isEmpty()) {
+        //     String s = in.readString();
+        //     if (trie.contains(s)) {
+        //         int t = trie.get(s);
+        //     }
+        // }
+        // StdOut.println("Trie took : " + sw.elapsedTime());
+    }
+}
