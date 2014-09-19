62d37386474f5b810d31d4cc726920b278aa51e2
MatthewPhinney <matthew.d.phinney@gmail.com>
2014-09-19 11:58:28 -0400

    Updated delete and max/min methods.  Still needs work.

--- java/algorithms/5.2/HybridTST.java
+++ java/algorithms/5.2/HybridTST.java
@@ -1,4 +1,5 @@
 import java.util.NoSuchElementException;
+import java.util.Iterator;
 public class HybridTST<Value>
 {
     private static final int R = 256;
@@ -16,14 +17,18 @@ public class HybridTST<Value>
         }
     }
 
+    // Returns true if the symbol table is empty
     public boolean isEmpty() {
         return N == 0;
     }
 
+    // Returns the number of key-value mappings in the symbol table
     public int size() {
         return N;
     }
 
+    // Adds the key-value pair to the symbol table if not already present
+    // Updates the value corresponding to the key, if the key is in the table already
     public void put(String key, Value val) {
         if (key == null || key.isEmpty()) throw new IllegalArgumentException("Key must be a non-empty String");
         if (!contains(key)) N++;
@@ -50,6 +55,7 @@ public class HybridTST<Value>
         return x;
     }
 
+    // Returns the value mapped to the given key.  Returns null if they key is not in the table.
     public Value get(String key) {
         if (key == null || key.isEmpty()) throw new IllegalArgumentException("Key must be a non-empty String");
         char c = key.charAt(0);
@@ -68,10 +74,12 @@ public class HybridTST<Value>
         else return x;
     }
 
+    // Returns true if the symbol table contains the given key
     public boolean contains(String key) {
         return get(key) != null;
     }
 
+    // Returns the minimum key in the symbol table (follows standard Java ordering for Strings)
     public String firstKey() {
         if (isEmpty()) throw new NoSuchElementException("symbol table is empty");
         StringBuilder sb = new StringBuilder();
@@ -89,6 +97,7 @@ public class HybridTST<Value>
         if (x.right != null) min(x.right, sb);
     }
 
+    // Returns the maximum key in the symbol table (follows standard Java ordering for Strings)
     public String lastKey() {
         if (isEmpty()) throw new NoSuchElementException("symbol table is empty");
         StringBuilder sb = new StringBuilder();
@@ -106,35 +115,33 @@ public class HybridTST<Value>
         if (x.left != null) max(x.left, sb);
     }
 
+    // Removes the key (and corresponding value) from the symbol table
     public void delete(String key) {
-        if (key == null || key.isEmpty()) throw new IllegalArgumentException("Key must be a non-empty String");
+        if (key == null) throw new NullPointerException("Null keys are disallowed");
+        if (key.isEmpty()) throw new IllegalArgumentException("Key must be a non-empty String");
         char c = key.charAt(0);
         root[c] = delete(root[c], key, 1);
     }
 
     private Node delete(Node x, String key, int d) {
-        StdOut.println(x + ": " + x.left + ": " + x.mid + ": " + x.right);
+        StdOut.println(x);
         if (x == null) return null;
-        if (d == key.length()) {
+        if (d == key.length()-1) {
             if (x.val != null) N--;
             x.val = null;
-        } else {
+        }
+        else {
             char c = key.charAt(d);
             if (c < x.c) x.left = delete(x.left, key, d);
             else if (c > x.c) x.right = delete(x.right, key, d);
-            else x.mid = delete(x.mid, key, d+1);
+            else if (d < key.length()-1) x.mid = delete(x.mid, key, d+1);
         }
-        //        StdOut.println(x);
-        if (x.val != null || x.mid != null) return x;
-        if (x.left == null) return x.right;
-        if (x.right == null) return x.left;
-        if (x.right != null && x.left != null) {
-            if (x.right.c - x.c > x.c - x.left.c) return x.left;
-            else return x.right;
-        }
+        if (x.val != null) return x;
+        if (x.left != null || x.mid != null || x.right != null) return x;
         return null;
     }
-    
+
+    // Returns an Iterable object that contains all the keys in the table
     public Iterable<String> keys() {
         Queue<String> q = new Queue<String>();
         for (char c = 0; c<R; c++) {
@@ -151,16 +158,62 @@ public class HybridTST<Value>
         traverse(x.right, m, q);
     }
 
+    // Test client
     public static void main(String[] args) {
         HybridTST<Integer> tst = new HybridTST<Integer>();
-        while (!StdIn.isEmpty()) {
-            String s = StdIn.readString();
-            if (tst.contains(s)) tst.put(s, tst.get(s) + 1);
-            else tst.put(s, 1);
+        assert(tst.isEmpty());
+
+        // Build table from standard input
+        // Test contains(String key), isEmpty(), put(String key, Value val), get(String key), size()
+        {
+            while (!StdIn.isEmpty()) {
+                String s = StdIn.readString();
+                if (tst.contains(s)) {
+                    int N = tst.size();
+                    int m = tst.get(s);
+                    tst.put(s, m + 1);
+                    assert(tst.size() == N);
+                    assert(tst.get(s) == m+1);
+                }
+                else {
+                    int N = tst.size();
+                    tst.put(s, 1);
+                    assert(tst.size() == N+1);
+                    assert(tst.contains(s));
+                    assert(tst.get(s) == 1);
+                }
+            }
         }
-        //        for (String s : tst.keys()) StdOut.println(s + ": " + tst.get(s));
-        StdOut.println();
-        tst.delete("sea");
-        //        for (String t : tst.keys()) StdOut.println(t + ": " + tst.get(t));
+        
+        // Check that we iterate in natural order
+        // Test keys(), firstKey(), lastKey()
+        {
+            Iterator<String> itr = tst.keys().iterator();
+            int i = 0;
+            int k = tst.size();
+            while (itr.hasNext()) {
+                String s = itr.next();
+                if (i == 0) assert(s.equals(tst.firstKey()));
+                else if (i == k-1) assert(s.equals(tst.lastKey()));
+                assert(tst.contains(s));
+                i++;
+            }
+        }
+
+        // Check delete(String key), firstKey(), lastKey()
+        {
+            while (!tst.isEmpty()) {
+                int k = tst.size();
+                String f = tst.firstKey();
+                String l = tst.lastKey();
+                StdOut.println(l);
+                tst.delete(l);
+                for (String s : tst.keys()) StdOut.print(s + " ");
+                StdOut.println();
+                assert(!tst.contains(l));
+                assert(tst.size() == k-1);
+                assert(tst.firstKey().equals(f));
+            }
+        }
     }
 }
