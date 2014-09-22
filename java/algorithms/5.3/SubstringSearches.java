632220af9972c0b9507c8e8e7ac205b162421a28
MatthewPhinney <matthew.d.phinney@gmail.com>
2014-09-22 11:27:05 -0400

    Added KMP algorithm

--- java/algorithms/5.3/SubstringSearches.java
+++ java/algorithms/5.3/SubstringSearches.java
@@ -12,13 +12,30 @@ public class SubstringSearches
     }
 
     public static int KMP(String pat, String txt) {
-        return -1;
+        int M = pat.length(), N = txt.length();
+        final int R = 256;
+        int[][] dfa = new int[R][M];
+        dfa[pat.charAt(0)][0] = 1;
+        int X;
+        int j;
+        int i;
+        for (j = 1, X = 0; j<M; j++) {
+            for (int c = 0; c<R; c++) dfa[c][j] = dfa[c][X];
+            dfa[pat.charAt(j)][j] = j+1;
+            X = dfa[pat.charAt(j)][X];
+        }
+        for (i = 0, j = 0; i<N && j<M; i++) {
+            j = dfa[txt.charAt(i)][j];
+        }
+        if (j == M) return i-M;
+        return N;
     }
+        
 
     public static void main(String[] args) {
         String pat = args[0];
         String txt = args[1];
-        int i = bruteForce(pat, txt);
+        int i = KMP(pat, txt);
         StdOut.println(i);
         StdOut.println(txt.substring(i, i+pat.length()));
     }
