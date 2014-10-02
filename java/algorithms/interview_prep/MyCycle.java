4b60e7a9f58aeb4d418bdcbafa4d2d1daa0a3689
MatthewPhinney <matthew.d.phinney@gmail.com>
2014-10-01 19:19:07 -0600

    Fixed cycle implementation to ignore reverse edge

--- java/algorithms/interview_prep/MyCycle.java
+++ java/algorithms/interview_prep/MyCycle.java
@@ -1,26 +1,28 @@
 public class MyCycle
 {
     private boolean[] marked;
-    private Iterable<Integer> cycle;
+    private Stack<Integer> cycle;
     private int[] edgeTo;
 
 
     public MyCycle(MyGraph G) {
         marked = new boolean[G.V()];
-        for (int v = 0; v<G.V(); v++) if (!marked[v]) dfs(G, v);
+        edgeTo = new int[G.V()];
+        for (int v = 0; v<G.V(); v++) if (!marked[v]) dfs(G, -1, v);
     }
 
-    private void dfs(MyGraph G, int v) {
+    private void dfs(MyGraph G, int s, int v) {
         marked[v] = true;
         for (int w : G.adj(v)) {
-            if (cycle != null) return;
+            if (hasCycle()) return;
             if (!marked[w]) {
                 edgeTo[w] = v;
-                dfs(G, w);
-            } else if (w{//need to ignore reverse edge back to v
+                dfs(G, v, w);
+            } else if (s != w) {
                 cycle = new Stack<Integer>();
+                for (int x = v; x != w; x = edgeTo[x]) cycle.push(x);
                 cycle.push(w);
-                for (int x = edgeTo[w]; x != w; x = edgeTo[w]) cycle.push(x);
+                cycle.push(v);
             }
         }
     }
@@ -31,5 +33,14 @@ public class MyCycle
 
     public Iterable<Integer> cycle() {
         return cycle;
+    }
+
+    public static void main(String[] args) {
+        MyGraph G = new MyGraph(new In(args[0]));
+        MyCycle c = new MyCycle(G);
+        if (c.hasCycle()) {
+            for (int v : c.cycle()) StdOut.print(v + " ");
+            StdOut.println();
+        }
     }
 }
