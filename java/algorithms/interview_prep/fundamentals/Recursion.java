c00259193327c229858a995c52b52664e3e6dd80
MatthewPhinney <matthew.d.phinney@gmail.com>
2014-10-08 14:16:34 -0700

    Euclid's algorithm

--- java/algorithms/interview_prep/fundamentals/Recursion.java
+++ java/algorithms/interview_prep/fundamentals/Recursion.java
@@ -3,7 +3,7 @@ public class Recursion
     public static void main(String[] args) {
         int N = Integer.parseInt(args[0]);
         int T = Integer.parseInt(args[1]);
-        factorialTimer(N, T);
+        StdOut.println(gcd(N, T));
     }
 
     
@@ -28,5 +28,10 @@ public class Recursion
         long fac = 1;
         for (int i = 1; i<=N; i++) fac *= i;
         return fac;
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a%b);
     }
 }
