f2242c4d0c493ffa6251222ae1ac85c0b42c74ce
MatthewPhinney <matthew.d.phinney@gmail.com>
2014-10-08 14:52:52 -0700

    Beginning implementation of BST

---
+++ java/algorithms/interview_prep/fundamentals/BinarySearchTree.java
@@ -0,0 +1,78 @@
+public class BinarySearchTree<Key extends Comparable<Key>, Value>
+{
+    private Node root;
+
+    private class Node
+    {
+        private Key key;
+        private Value val;
+        private Node left, right;
+        private int N;
+
+        public Node(Key key, Value val, int N) {
+            this.key = key; this.val = val; this.N = N;
+        }
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public int size() {
+        return size(root);
+    }
+
+    private int size(Node x) {
+        if (x == null) return 0;
+        return x.N;
+    }
+
+    public void put(Key key, Value val) {
+        root = put(root, key, val);
+    }
+
+    private Node put(Node x, Key key, Value val) {
+        if (x == null) return new Node(key, val, 1);
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) x.left = put(x.left, key, val);
+        else if (cmp > 0) x.right = put(x.right, key, val);
+        else x.val = val;
+        x.N = 1 + size(x.left) + size(x.right);
+        return x;
+    }
+
+    public Value get(Key key) {
+        Node x = get(root, key);
+        if (x == null) return null;
+        return x.val;
+    }
+
+    private Node get(Node x, Key key) {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) return get(x.left, key);
+        else if (cmp > 0) return get(x.right, key);
+        else return x;
+    }
+
+    public boolean contains(Key key) {
+        return get(key) != null;
+    }
+
+    public void delete(Key key) {
+        root = delete(root, key);
+    }
+
+    private Node delete(Node x, Key key) {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) x.left = delete(x.left, key);
+        else if (cmp > 0) x.right = delete(x.right, key);
+        else {
+            if (x.left == null) return x.right;
+            if (x.right == null) return x.left;
+        }
+        x.N = 1 + size(x.left) + size(x.right);
+        return x;
+    }
+}
