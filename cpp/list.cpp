d3f9047a53dd8b456a8c55eaf8ab4a0814ef070d
MatthewPhinney <matthew.d.phinney@gmail.com>
2014-10-12 10:49:22 -0700

    Added functionality to linked list class

--- cpp/list.cpp
+++ cpp/list.cpp
@@ -1,24 +1,64 @@
 #include<iostream>
 #include<stdlib.h>
 using namespace std;
-struct Node {
+class Node {
+public:
+  Node();
+  ~Node();
+  void add(Node* p);
+  void set_data(int d);
+  void set_next(Node* n);
+  int get_data();
+  Node* get_next();
+private:
+  Node* head;
   int data;
   Node* next;
 };
 
-int main(int numarg, char* argv[]) {
-  Node* head = NULL;
-  int N = atoi(argv[1]);
-  for (int i = 0; i<N; i++) {
-    Node* tmp = new Node;
-    tmp -> data = i;
-    tmp -> next = head;
-    head = tmp;
-  }
+Node::Node()
+{
+}
 
-  Node* current = head;
-  while (NULL != current) {
-    cout<< current -> data<<endl;
-    current = current -> next;
-  }
+Node::~Node()
+{
+}
+
+Node* Node::get_next() {
+  return this -> next;
+}
+
+int Node::get_data() {
+  return this -> data;
+}
+
+void Node::set_next(Node* n) {
+  this -> next = n;
+}
+
+void Node::set_data(int d) {
+  this -> data = d;
+}
+
+void Node::add(Node* p) {
+  p -> next = head;
+  head = p;
+}
+  
+int main(int numarg, char* argv[]) {
+  // Node* head = NULL;
+  // int N = atoi(argv[1]);
+  // for (int i = 0; i<N; i++) {
+  //   Node* tmp = new Node;
+  //   tmp -> data = i;
+  //   tmp -> next = head;
+  //   head = tmp;
+  // }
+
+  // Node* current = head;
+  // while (NULL != current) {
+  //   cout<< current -> data<<endl;
+  //   current = current -> next;
+  // }
+  return 0;
 }
