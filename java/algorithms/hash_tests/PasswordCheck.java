982de6931dc677c300c1c80f5ba29288d3ebd5d3
MatthewPhinney <matthew.d.phinney@gmail.com>
2014-11-01 17:10:47 -0700

    Don't allow the reverse of dictionary words

--- java/algorithms/hash_tests/PasswordCheck.java
+++ java/algorithms/hash_tests/PasswordCheck.java
@@ -15,11 +15,25 @@ public class PasswordCheck
        return true;
     }
 
+    private static String reverse(String s) {
+        char[] a = s.toCharArray();
+        int N = a.length;
+        for (int i = 0; i<=N/2; i++) {
+            char t = a[i];
+            a[i] = a[N-1-i];
+            a[N-1-i] = t;
+        }
+        return new String(a);
+    }
+
     public static void main(String[] args) {
         String s = args[0];
         HashSet<String> dictionary = new HashSet<String>();
         while (!StdIn.isEmpty()) {
-            dictionary.add(StdIn.readString());
+            String p = StdIn.readString();
+            String r = reverse(s);
+            dictionary.add(p);
+            dictionary.add(r);
         }
         if (goodPassword(s, dictionary)) {
             StdOut.println("Your password is OK");
